4f4c7e93445b49b6d8e7743c4d318edcef9d30c5
satya3 <satyavemuri.ui@gmail.com>
2016-02-10 14:12:59 -0800

    this my Bootstap project initial commit

---
+++ new.php
@@ -0,0 +1,13 @@
+<?php
+$connection = mysql_connect("localhost", "root", ""); // Establishing Connection with Server..
+$db = mysql_select_db("mydba", $connection); // Selecting Database
+//Fetching Values from URL
+$name2=$_POST['name1'];
+$email2=$_POST['email1'];
+$password2=$_POST['password1'];
+$contact2=$_POST['contact1'];
+//Insert query
+$query = mysql_query("insert into form_element(name, email, password, contact) values ('$name2', '$email2', '$password2','$contact2')");
+echo "Form Submitted Succesfully";
+mysql_close($connection); // Connection Closed
+?>
